693187b85ea88f243d3917419850a0f71602a9f9
palvarzimaz <palvarezimaz@gmail.com>
2022-09-07 00:26:14 +1000

    tag manager

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import ReactGA from 'react-ga4';
+import TagManager from 'react-gtm-module';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
@@ -11,6 +12,14 @@ import './App.scss';
 
 // const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
 // ReactGA.initialize(TRACKING_ID);
+
+const TAG_ID = process.env.REACT_APP_TAG_ID;
+
+const tagManagerArgs = {
+  gtmId: TAG_ID,
+};
+
+TagManager.initialize(tagManagerArgs);
 
 function App() {
   return (
