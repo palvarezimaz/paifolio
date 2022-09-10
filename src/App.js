e0a10d355691c3c2e5eb14ea1cb654693130440f
palvarzimaz <palvarezimaz@gmail.com>
2022-09-11 01:29:05 +1000

    GA working, code cleanup

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import React from 'react';
-
 import ReactGA from 'react-ga4';
-// import TagManager from 'react-gtm-module';
-
-
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-
 import Homepage from './pages/Homepage';
-
-// import RouteChangeTracker from './helpers/RouteChangeTracker';
-
 import './App.scss';
 
 const TRACKING_ID = process.env.REACT_APP_TRACKING_ID;
 ReactGA.initialize(TRACKING_ID);
 
-// const TAG_ID = process.env.REACT_APP_TAG_ID;
-
-// const tagManagerArgs = {
-//   gtmId: TAG_ID,
-// };
-
-// TagManager.initialize(tagManagerArgs);
-
 function App() {
-  // usePageTracking();
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
@@ -32,9 +15,6 @@ function App() {
           <Homepage />
         </Route>
       </Switch>
-      {/* <div>
-        <RouteChangeTracker />
-      </div> */}
     </BrowserRouter>
   );
 }
